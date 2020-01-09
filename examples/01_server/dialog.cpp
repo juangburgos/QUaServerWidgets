df8859ad9862195121536800953a47ff8592e27c
juangburgos <juangburgos@gmail.com>
2020-01-09 15:04:40 +0100

    Added some OPC methods for testing UI updates

--- examples/01_server/dialog.cpp
+++ examples/01_server/dialog.cpp
@@ -8,6 +8,59 @@ Dialog::Dialog(QWidget *parent)
     ui->setupUi(this);
     // bind server to widget
     ui->widgetServer->bindServer(&m_server);
+
+    // setup test methods
+    auto objsFolder = m_server.objectsFolder();
+    // 
+    objsFolder->addMethod("setPort",
+    [this](quint16 intPort) {
+        m_server.setPort(intPort);
+    });
+    // 
+    objsFolder->addMethod("setMaxSecureChannels",
+    [this](quint16 maxSecureChannels) {
+        m_server.setMaxSecureChannels(maxSecureChannels);
+    });
+    // 
+    objsFolder->addMethod("setMaxSessions",
+    [this](quint16 maxSessions) {
+        m_server.setMaxSessions(maxSessions);
+    });
+    // 
+    objsFolder->addMethod("setApplicationName",
+    [this](QString strApplicationName) {
+        m_server.setApplicationName(strApplicationName);
+    });
+    // 
+    objsFolder->addMethod("setApplicationUri",
+    [this](QString strApplicationUri) {
+        m_server.setApplicationUri(strApplicationUri);
+    });
+    // 
+    objsFolder->addMethod("setProductName",
+    [this](QString strProductName) {
+        m_server.setProductName(strProductName);
+    });
+    // 
+    objsFolder->addMethod("setProductUri",
+    [this](QString strProductUri) {
+        m_server.setProductUri(strProductUri);
+    });
+    // 
+    objsFolder->addMethod("setManufacturerName",
+    [this](QString strManufacturerName) {
+        m_server.setManufacturerName(strManufacturerName);
+    });
+    // 
+    objsFolder->addMethod("setSoftwareVersion",
+    [this](QString strSoftwareVersion) {
+        m_server.setSoftwareVersion(strSoftwareVersion);
+    });
+    // 
+    objsFolder->addMethod("setBuildNumber",
+    [this](QString strBuildNumber) {
+        m_server.setBuildNumber(strBuildNumber);
+    });
 }
 
 Dialog::~Dialog()
